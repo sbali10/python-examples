e9f88f31527abaf58835c35685b040f257176ecb
Serhat Bali <serhat94@outlook.com>
2021-02-20 16:54:33 +0300

    first development

---
+++ btk_python/03-index.py
@@ -0,0 +1,25 @@
+'''
+#sehirler = ['izmir', 'diyarbakır']
+#plakalar = [35, 21]
+#print(plakalar[sehirler.index('izmir')])
+
+
+
+#plakalar = { 'izmir' : 35, 'diyarbakır': 21}
+#print(plakalar['izmir'])
+
+users = {
+    'serhatbali': {
+        'age': 10,
+        'gender': 'male',
+        'role': 'admin',
+        'phone' : '321456'
+    }
+}
+
+print(users['serhatbali'])
+print(users['serhatbali']['phone'])
+print(users['serhatbali']['role'])
+
+'''
+
---
+++ btk_python/Student-info.py
@@ -0,0 +1,74 @@
+#Creating new user profile for a student or get user info of a existing student
+
+
+select = input("Hello if you're our student select 1, if not select 2 : ")
+select = int(select)
+print(select)
+
+
+
+if select == 1:
+
+    student= {
+        '120': {
+            'name': 'Ali',
+            'surname': 'A',
+            'phone': '111 111'    
+        },
+        '121' : {
+            'name': 'Bali',
+            'surname': 'B',
+            'phone': '222 222'
+        },
+        '122': {
+            'name': 'CAli',
+            'surname': 'C',
+            'phone': '444 111'
+        }
+    
+    }
+
+    idk = input('Entry student number: ')   #öğrenci no girdirdik
+    stud = student[idk]               # öğrenci no'suna ait tablodaki index bulundu
+    
+    
+    #print(stud)                          #girilen öğrenci noya ait tüm bilgiler yazdırılı
+    print('*'*50)                          
+    print(f"The name of your student is {((stud)['name'])} and surname is {((stud)['surname'])}. Contact number of your student is {((stud)['phone'])}")
+    print('*'*50)
+    
+
+
+
+
+elif select == 2:
+
+    student = {}
+
+    number = input("Student number: ")
+    name = input("Student name: ")
+    surname = input("Student surname: ")
+    phone = input("Student phone number: ")
+    
+    student.update({
+        number: {
+            'name': name,
+            'surname': surname,
+            'phone': phone
+    
+        }
+    })
+    
+    print('*'*50)
+    print(student)
+    print('*'*50)
+
+
+
+else:
+    print("Select 1 or 2 for your correct status")
+
+
+
+
+
