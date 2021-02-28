1de4552547fee9a9087801fc4be481e2bee353fa
Serhat Bali <serhat94@outlook.com>
2021-02-28 11:48:23 +0300

    fonk klasör ayrıldı

--- btk_python/7-Fonksiyonlar/yas-emeklilik-fonk.py
+++ btk_python/7-Fonksiyonlar/yas-emeklilik-fonk.py
@@ -1,30 +1,3 @@
-'''
-list = [1,2,3]
-
-#list.append(4)    #liste sonuna 4 ekler
-#lit.pop()         #listenin  son elemanını siler
-#print(type(list))    #listenin classının yazdırr burda list mesela
- 
-print(list)
-
-###############################################################################################################
-
-def sayHello(name = "user"):               #Hello + girilen isimi yazan fonksiyon
-    print("Hello " + name)
-
-sayHello('Serhat')    #name yerine atanır
-#sayHello()            #name yerine birşey girilmezse "user" kelimesini otomatik atar
-
-###############################################################################################################
-
-def total(num1,num2):       #İki sayıyıyı toplayan fonskiyon
-    return num1 + num2
-
-result = total(10,20)
-print(result)
-
-'''
-
 def yasHesapla(dogumYili):            
 
     '''
