ac053f3da78f47c5c912904380bf21169156abf8
Serhat Bali <serhat94@outlook.com>
2021-03-08 22:48:10 +0300

    update

---
+++ OOP/deneme.py
@@ -0,0 +1,17 @@
+'''
+#SB deneme class ve method
+
+class Arabalar():
+    def __init__(self,marka,model,renk,vites):
+
+        self.marka  = marka
+        self.model  = model
+        self.renk   = renk
+        self.vites  = vites
+
+
+araba_1 = Arabalar("Ford", "Mustang", "Siyah", "Manuel")
+
+print(araba_1.marka, araba_1.model, araba_1.renk, araba_1.vites)
+
+'''
