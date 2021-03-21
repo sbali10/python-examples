fb464b66ce302c0747062e7f5c62e6d113e1af6a
Serhat Bali <serhat94@outlook.com>
2021-03-21 16:01:18 +0300

    obs created

--- Work/Folder-Read/files.py
+++ Work/Folder-Read/files.py
@@ -1,13 +1,15 @@
-# #folder oluşturma(w yazma modu)
+# #folder oluşturma(w yazma modu)(r ouma modu)(a append ekleme modu cursoru sona atar)
 # #file = open("C:\Users\serha\OneDrive\Masaüstü\newfile.txt", "w")    #masaüstüne yeni dosya oluşturma
 
 # #dosya oluşturma ve içine türkçe karakterde olsa yazdırma(dosya varsa içeriğini sile bunları ekler)
-# file = open("newfile.txt","w",encoding='utf-8')
+# file = open("Work\Folder-Read\newfile.txt","w",encoding='utf-8')
 # file.write("Serhat Bali")
 # file.close()
 
-########################################################################################################33
+########################################################################################################
 # #append ile ekleme (yeni değer ekleme ve değeri istenirse alt satıra)
-# file = open("newfile.txt","a",encoding='utf-8')
+# file = open("Work\Folder-Read\newfile.txt","a",encoding='utf-8')
 # file.write("Serhat Bali \n")
 # file.close()
+
+########################################################################################################
--- Work/Folder-Read/reading.py
+++ Work/Folder-Read/reading.py
@@ -1,6 +1,6 @@
 
 # try:
-#     file = open("newfile.txt","r")
+#     file = open("Work\Folder-Read\newfile.txt","r")
 #     print(file)
 # except FileNotFoundError:
 #     print("dosya okuma hatası")
@@ -9,7 +9,7 @@
 #     file.close()
 
 
-file = open("newfile.txt","r", encoding="utf-8")
+file = open("Work\Folder-Read\newfile.txt","r", encoding="utf-8")
 
 #################################################
 # for i in file:
@@ -29,10 +29,48 @@ file = open("newfile.txt","r", encoding="utf-8")
 #file.close()
 
 ######################################################################################################
-with open("newfile.txt","r",encoding="utf-8") as file:          #file.close kullnamamak için with kullandık
-    content = file.read()
-    print(content)
-    file.seek(5)                #cursorü 5.karakter konumuna gönderdik
-    print(file.tell())           #cursorün nerede olduğunu gösterir
-    content2 = file.read()
-    print(content2)               #5.satırda bıraktığımız cursörden geri kalanları yazdırdık
+
+###sayfa başında güncelleme###
+
+# with open("Work\Folder-Read\newfile.txt","r+",encoding="utf-8") as file:          #file.close kullnamamak için with kullandık
+#     content = file.read()
+#     content = "ÖBBali\n" + content
+#     file.write(content)
+#     #file.seek(0)                #cursorü 0.karakter konumuna gönderdik
+#     #print(file.tell())           #cursorün nerede olduğunu gösterir
+#     #content2 = file.read()
+#     #print(content2)               #5.satırda bıraktığımız cursörden geri kalanları yazdırdık
+
+
+# with open("Work\Folder-Read\newfile.txt","r",encoding="utf-8") as file:              #dosya başına ekleme 
+#     print(file.read())
+
+################################################################################################################
+
+###sayfa sonunda güncelleme###
+
+# with open("Work\Folder-Read\newfile.txt","a", encoding="utf-8") as file:             #dosya sonun ekleme yapar
+#     file.write("\nCBali")
+
+# with open("Work\Folder-Read\newfile.txt", "r", encoding="utf-8") as file:            #içine yazdığımız dosyayı ekrana yazdırır
+#     print(file.read())
+
+
+################################################################################################################
+
+###Sayfa ortasına ekleme yapma###
+
+# with open("Work\Folder-Read\newfile.txt", "r+", encoding="utf-8") as file:
+#     list = file.readlines()
+#     list.insert(1,"OrtayaEkle2")
+#     file.seek(0)
+#     file.writelines(list)
+#     # print(list)
+#     # for i in list:
+#     #     file.write(i)
+
+# with open("Work\Folder-Read\newfile.txt", "r", encoding="utf-8") as file:
+#     print(file.read())
+
+################################################################################################################
+
